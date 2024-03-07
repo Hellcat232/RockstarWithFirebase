f70587c8299443d810363c7bf13c9900300f8602
Hellcat232 <maxkuich1993@gmail.com>
2024-03-07 13:32:44 +0200

    f

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyCzuIGq0L3Cq_G00aTQjH7wZAtFYk0b4bY',
+  authDomain: 'rockstar-efa35.firebaseapp.com',
+  projectId: 'rockstar-efa35',
+  storageBucket: 'rockstar-efa35.appspot.com',
+  messagingSenderId: '510809606086',
+  appId: '1:510809606086:web:37c769dc38d9b5493e54c7',
+  measurementId: 'G-B3GTR6DE2F',
+});
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
+onAuthStateChanged(auth, user => {
+  if (user !== null) {
+    console.log('Logged in!');
+  } else {
+    console.log('No user!');
+  }
+});
