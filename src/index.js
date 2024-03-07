4c8926a084e9c7a00f9bbbb7bc6513b33b36d48f
Hellcat232 <maxkuich1993@gmail.com>
2024-03-07 14:16:59 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {
+  getAuth,
+  onAuthStateChanged,
+} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
 
-const firebaseApp = initializeApp({
+const firebaseConfig = {
   apiKey: 'AIzaSyCzuIGq0L3Cq_G00aTQjH7wZAtFYk0b4bY',
   authDomain: 'rockstar-efa35.firebaseapp.com',
   projectId: 'rockstar-efa35',
@@ -10,10 +13,10 @@ const firebaseApp = initializeApp({
   messagingSenderId: '510809606086',
   appId: '1:510809606086:web:37c769dc38d9b5493e54c7',
   measurementId: 'G-B3GTR6DE2F',
-});
-
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseConfig);
+const db = getFirestore(app);
 
 onAuthStateChanged(auth, user => {
   if (user !== null) {
